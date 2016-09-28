11cffae5245198cc3f0a1711a58772f96e0d5fcb
Oleg <oleglustenko@gmail.com>
2016-09-29 01:28:28 +0300

    home work 5

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,19 @@
-let gulp = require('gulp');
-let postcss = require('gulp-postcss');
-let sourcemaps = require('gulp-sourcemaps');
-let rename = require('gulp-rename');
-// let {join} = require('path');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
 const cssPath = ['./src/**/*.css', '!./src/**/*.next.css'];
 
 
 gulp.task('postcss', function () {
   gulp.src(cssPath)
-    .pipe(postcss([require('postcss-cssnext')({ browsers: null })]))
+    .pipe(postcss([require('postcss-font-magician')(),
+      require('postcss-cssnext')({ browsers: null })
+    ]))
     .pipe(rename({ suffix: '.next' }))
     .pipe(gulp.dest((f) => {
-      // console.log(JSON.stringify(f));
-      let history = f.history[0].split('/'), filename = 'next.' + history.pop();
-      // history.pop(); history.push('dist/'); history.splice(0, 5);
-      console.log(filename);
-      // return './' + history.join('/') + filename;
+      console.log(f.history[0].split('/').pop());
       return f.base;
     }));
 });
 
-gulp.task('default', ['postcss'], function () {
-  gulp.watch(cssPath, ['postcss']);
-  }
-);
+gulp.task('default', ['postcss'], () => { gulp.watch(cssPath, ['postcss']); });
