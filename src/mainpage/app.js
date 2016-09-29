45e6159b84ebfb5aa7ad92f43eb7acb19d16ff52
Oleg <oleglustenko@gmail.com>
2016-09-29 23:34:10 +0300

    small update

---
+++ src/mainpage/app.js
@@ -0,0 +1,17 @@
+const Component = React.Component;
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
